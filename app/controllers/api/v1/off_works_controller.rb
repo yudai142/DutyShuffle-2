5de2169bb7b85a564755df15416f1e3545f05792
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-04-21 22:37:09 +0900

    fix(issue-40): OffWorks コントローラでワークシート ID パラメータに対応
    - index (fetch_off_works): current_worksheet を current_worksheet_for_params に変更
    - create: worksheet_id パラメータを使用してワークシートを取得
    - destroy: worksheet_id パラメータを使用してワークシートを取得
    - デモアカウント以外で複数ワークシート時のデータ読み込み失敗を修正

--- app/controllers/api/v1/off_works_controller.rb
+++ app/controllers/api/v1/off_works_controller.rb
@@ -9,13 +9,15 @@ module Api
       end
 
       def create
-        @off_work = current_worksheet.off_works.build(off_work_params)
+        worksheet = current_worksheet_for_params
+        @off_work = worksheet.off_works.build(off_work_params)
         @off_work.save!
         render json: @off_work, serializer: OffWorkSerializer, status: :created
       end
 
       def destroy
-        @off_work = current_worksheet.off_works.find(params[:id])
+        worksheet = current_worksheet_for_params
+        @off_work = worksheet.off_works.find(params[:id])
         @off_work.destroy!
         head :no_content
       end
@@ -23,7 +25,7 @@ module Api
       private
 
       def fetch_off_works
-        query = current_worksheet.off_works
+        query = current_worksheet_for_params.off_works
         query = query.where(date: params[:date]) if params[:date].present?
         @off_works = query
       end
